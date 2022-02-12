98d0038709b059a0d065817c44e3ea1c8ff0cd39
Piotr Gębalski <piotrgebalski5@gmail.com>
2022-02-12 17:52:30 +0100

    Dodaj pliki projektów.

---
+++ Upel_Scrap/GetToken.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using Newtonsoft.Json;
+
+
+namespace Upel_Scrap
+{
+    class GetToken
+    {
+        public static string GetSecurityToken(string document)
+        {
+            List<string> list = document.Split(">").ToList();
+            var str = list.FirstOrDefault(s => s.Contains("name=\"logintoken\""));
+            str = str.Trim();
+            str = str.Substring(str.Length - 33);
+            str = str.Remove(str.Length - 1);
+            return str;
+        }
+    }
+}
---
+++ Upel_Scrap/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Flurl.Http;
+using System.Collections.Specialized;
+using System.Net;
+using System.Text;
+using HtmlAgilityPack;
+
+
+namespace Upel_Scrap
+{
+
+    class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var html = await httpClient.GetAsync("https://upel2.cel.agh.edu.pl/wfiis/login/index.php");
+                var html_string = await html.Content.ReadAsStringAsync();
+
+                var token = GetToken.GetSecurityToken(html_string);
+                Console.Write("Insert Login: ");
+                string login = Console.ReadLine();
+                Console.Write("Insert Password: ");
+                string password = Console.ReadLine();
+
+                var values = new Dictionary<string, string>
+                {
+                {"anchor", "" },
+                {"logintoken", token },
+                {"username", login },
+                {"password", password }
+                };
+                var content = new FormUrlEncodedContent(values);
+
+                var response = await httpClient.PostAsync("https://upel2.cel.agh.edu.pl/wfiis/login/index.php", content);
+                var response_text = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(response_text);
+
+            }
+        }
+    }
+}
+
