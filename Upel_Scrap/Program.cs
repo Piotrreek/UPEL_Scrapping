7a2bf8aa88e904736df18c6d3f6bb5d3edbf27a8
Piotr Gębalski <piotrgebalski5@gmail.com>
2022-02-13 14:03:41 +0100

    Added some functions to let the code look better

--- Upel_Scrap/Program.cs
+++ Upel_Scrap/Program.cs
@@ -9,67 +9,36 @@ namespace Upel_Scrap
 {
     class Program
     {
-        private static readonly HttpClient client = new HttpClient();
+        private static HttpClient client = new HttpClient();
+
         static async Task Main(string[] args)
         {
-            using (var httpClient = new HttpClient())
+            using var httpClient = new HttpClient();
+            var Html = await httpClient.GetAsync("https://upel2.cel.agh.edu.pl/wfiis/login/index.php");
+            var HtmlString = await Html.Content.ReadAsStringAsync();
+            var Token = SignToWebsite.GetSecurityToken(HtmlString);
+            var LoginAndPassword = SignToWebsite.GetLoginAndPassword();
+            var Response = await SignToWebsite.SignIn(Token, LoginAndPassword, httpClient);
+
+            int choice = Scrapping.Choose();
+            do
             {
-                var html = await httpClient.GetAsync("https://upel2.cel.agh.edu.pl/wfiis/login/index.php");
-                var html_string = await html.Content.ReadAsStringAsync();
-
-                // getting security token to log in to the website
-                var token = GetToken.GetSecurityToken(html_string);
-
-                Console.Write("Insert Login: ");
-                string login = Console.ReadLine();
-
-                // inserted password will not be seen in the console
-                Console.Write("Insert Password: ");
-                string password = null;
-                while (true)
+                switch (choice)
                 {
-                    var key = Console.ReadKey(true);
-                    if (key.Key == ConsoleKey.Enter)
+                    case 1:
+                        await Scrapping.PpoGrades(httpClient);
                         break;
-                    password += key.KeyChar;
+                    case 2:
+
+                        break;
+                    case 5:
+                        return;
+                    default:
+                        Console.WriteLine("Bad input, try again");
+                        break;
                 }
-                // values to post to the webiste
-                var values = new Dictionary<string, string>
-                {
-                {"anchor", "" },
-                {"logintoken", token },
-                {"username", login },
-                {"password", password }
-                };
-                var content = new FormUrlEncodedContent(values);
-
-                var response = await httpClient.PostAsync("https://upel2.cel.agh.edu.pl/wfiis/login/index.php", content); 
-                var response_text = await response.Content.ReadAsStringAsync();
-
-                // string containing html code of the website after being logged in
-                // Console.WriteLine(response_text);
-
-                // getting code of PPO_COURSE Grades
-                var ppo_course = await httpClient.GetAsync("https://upel2.cel.agh.edu.pl/wfiis/grade/report/user/index.php?id=464");
-                var ppo_course_str = await ppo_course.Content.ReadAsStringAsync();
-
-                // creating new HtmlDocument from ppo_course_str
-                ppo_course_str = WebUtility.HtmlDecode(ppo_course_str);
-                HtmlDocument ppo_course_html = new HtmlDocument();
-                ppo_course_html.LoadHtml(ppo_course_str);
-
-
-                Console.WriteLine(ppo_course_str);
-
-                // now scrapping ... 
-
-
-                 
-                
-
-                
-
-            }
+                choice = Scrapping.Choose();
+            } while (true);
         }
     }
 }
---
+++ Upel_Scrap/SignToWebsite.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using Newtonsoft.Json;
+using System.Net;
+using System.Net.Http;
+
+
+namespace Upel_Scrap
+{
+    class SignToWebsite
+    {
+        public static string GetSecurityToken(string document)
+        {
+            List<string> List = document.Split(">").ToList();
+            var Str = List.FirstOrDefault(s => s.Contains("name=\"logintoken\""));
+            Str = Str.Trim();
+            Str = Str.Substring(Str.Length - 33);
+            Str = Str.Remove(Str.Length - 1);
+            return Str;
+        }
+
+        public static List<string> GetLoginAndPassword()
+        {
+            Console.Write("Insert Login: ");
+            string Login = Console.ReadLine();
+            // inserted password will not be seen in the console
+            Console.Write("Insert Password: ");
+            string Password = null;
+            while (true)
+            {
+                var Key = Console.ReadKey(true);
+                if (Key.Key == ConsoleKey.Enter)
+                    break;
+                Password += Key.KeyChar;
+            }
+            List<string> Data = new List<string> { Login, Password };
+            return Data;
+        }
+
+        public static async Task<HttpResponseMessage> SignIn(string token, List<string> login_password,  HttpClient httpClient)
+        {
+            var Values = new Dictionary<string, string>
+                {
+                {"anchor", "" },
+                {"logintoken", token },
+                {"username", login_password[0] },
+                {"password", login_password[1] }
+                };
+            var Content = new FormUrlEncodedContent(Values);
+            return  await httpClient.PostAsync("https://upel2.cel.agh.edu.pl/wfiis/login/index.php", Content);
+        }
+    }
+}
